d349b2cc47f1c2fd31c502f86f26e63ee1e230dd
AdamWalton-0 <waltona@merrimack.edu>
2025-11-06 14:13:18 -0500

    Update ACKPacket.java

--- Project3/src/tftp/ACKPacket.java
+++ Project3/src/tftp/ACKPacket.java
@@ -3,15 +3,16 @@ import java.nio.ByteBuffer;
 public class ACKPacket extends TFTPPacket {
     private short blockNumber;
 
-    public ACKPacket(short blockNumber){
+    public ACKPacket(short blockNumber) {
         super(OP_ACK);
         this.blockNumber = blockNumber;
     }
 
     public ACKPacket(byte[] data) {
         super(OP_ACK);
-        this.opcode = ByteBuffer.wrap(data, 0, 2).getShort();
-        this.blockNumber = ByteBuffer.wrap(data, 2, 2).getShort();
+        ByteBuffer buf = ByteBuffer.wrap(data);
+        this.opcode = buf.getShort();
+        this.blockNumber = buf.getShort();
     }
 
     @Override
@@ -20,5 +21,9 @@ public class ACKPacket extends TFTPPacket {
         buf.putShort(opcode);
         buf.putShort(blockNumber);
         return buf.array();
+    }
+
+    public short getBlockNumber() {
+        return blockNumber;
     }
 }
