25a3170723cd72716d7f3305f2679fb8eff04886
AdamWalton-0 <waltona@merrimack.edu>
2025-11-06 14:18:34 -0500

    Create TFTPErrorException.java

---
+++ Project3/src/tftp/TFTPErrorException.java
@@ -0,0 +1,11 @@
+
+
+public class TFTPErrorException extends Exception {
+    public TFTPErrorException(String message) {
+        super(message);
+    }
+
+    public TFTPErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
