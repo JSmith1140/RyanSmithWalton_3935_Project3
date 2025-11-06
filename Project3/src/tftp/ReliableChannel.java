dc805b6452b37cbc7b40f3d179d34516ba05349d
AntonRyan1 <ryanan@merrimack.edu>
2025-11-06 13:53:56 -0500

    Update ReliableChannel.java
    not done yet just starting code will work on it more but this is the start

--- Project3/src/tftp/ReliableChannel.java
+++ Project3/src/tftp/ReliableChannel.java
@@ -1,42 +1,43 @@
-
-package tftp;
-
-import java.io.*;
+import java.io.IOException;
 import java.net.*;
-import tftp.TftpCore;  
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ReliableChannel {
-    private final DatagramSocket socket;
-    private final boolean isServer;
 
-    public ReliableChannel(DatagramSocket socket, boolean isServer) {
-        this.socket = socket;
-        this.isServer = isServer;
+    private static final int MAX_TFTP_DATAGRAM = 4 + 512 + 4;
+
+    private static final int DEFAULT_TIMEOUT_MS = 2000;
+
+    private final DatagramSocket socket;
+    private SocketAddress pinnedPeer;
+
+    public ReliableChannel(DatagramSocket socket) {
+        this.socket = Objects.requireNonNull(socket, "socket");
     }
 
-    public void send(TftpCore.Packet pkt, SocketAddress peer) throws IOException {
+    public SocketAddress getPinnedPeer() {
+        return pinnedPeer;
+    }
+
+    public void setPinnedPeer(SocketAddress peer) {
+        this.pinnedPeer = peer;
+    }
+
+    public void send(TFTPPacket pkt, SocketAddress peer) throws IOException {
         byte[] data = pkt.serialize();
-        DatagramPacket dp = new DatagramPacket(data, data.length, peer);
+        DatagramPacket dp = new DatagramPacket(data, data.length, toInetSocketAddress(peer));
         socket.send(dp);
     }
 
-    public <T extends TftpCore.Packet> T receive(Class<T> expected, int timeoutMs)
-            throws IOException, TftpCore.TftpException {
-        socket.setSoTimeout(timeoutMs);
-        int retries = 0;
-        while (true) {
-            try {
-                byte[] buf = new byte[516]; 
-                DatagramPacket dp = new DatagramPacket(buf, buf.length);
-                socket.receive(dp);
-                TftpCore.Packet pkt = TftpCore.Packet.parse(dp);
-                if (expected.isInstance(pkt))
-                    return expected.cast(pkt);
-            } catch (SocketTimeoutException e) {
-                if (++retries >= 3)
-                    throw new TftpCore.TftpErrorException("Timeout: giving up after 3 retries.");
-            }
+    public void send(TFTPPacket pkt) throws IOException {
+        if (pinnedPeer == null) {
+            throw new IllegalStateException("Pinned peer is not set yet.");
         }
+        send(pkt, pinnedPeer);
+    }
+    public static class ProtocolException extends Exception {
+        public ProtocolException(String msg) { super(msg); }
     }
 }
-
