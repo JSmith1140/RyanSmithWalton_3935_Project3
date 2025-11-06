b736c868465cd71f264421983e69ae4e0d78f97d
JSmith1140 <168117141+JSmith1140@users.noreply.github.com>
2025-11-05 20:16:06 -0500

    DATAPacket subclass

---
+++ Project3/src/tftp/DATAPacket.java
@@ -0,0 +1,30 @@
+import java.nio.ByteBuffer;
+
+public class DATAPacket extends TFTPPacket {
+    private short blockNumber;
+    private byte[] data;
+
+    public DATAPacket(short blockNumber, byte[] data) {
+        super(OP_DATA);
+        this.blockNumber = blockNumber;
+        this.data = data;
+    }
+
+    public DATAPacket(byte[] data) {
+        super(OP_DATA);
+        ByteBuffer buf = ByteBuffer.wrap(data);
+        this.opcode = buf.getShort();      
+        this.blockNumber = buf.getShort();  
+        this.data = new byte[data.length - 4];
+        buf.get(this.data);
+    }
+
+    @Override
+    public byte[] serialize() {
+        ByteBuffer buf = ByteBuffer.allocate(4 + data.length);
+        buf.putShort(opcode);
+        buf.putShort(blockNumber);
+        buf.put(data);
+        return buf.array();
+    }
+}
