a09e6485400957d0ccd73907f635845c32da241f
JSmith1140 <168117141+JSmith1140@users.noreply.github.com>
2025-11-14 17:20:50 -0500

    Added javadoc comments to WRQPacket.java

--- Project3/src/tftp/WRQPacket.java
+++ Project3/src/tftp/WRQPacket.java
@@ -7,11 +7,28 @@ public class WRQPacket extends TFTPPacket {
     private String filename;
     private String mode = "octet";
 
+    /**
+     * Constructs a new WRQPacket requesting permission to write a file.
+     *
+     * @param filename the name of the file being uploaded to the server
+     */
     public WRQPacket(String filename) {
         super(OP_WRQ);
         this.filename = filename;
     }
 
+    /**
+     * Constructs a WRQPacket by parsing raw serialized packet data.
+     * <p>
+     * Expected format:
+     * <pre>
+     *     2 bytes: opcode (2)
+     *     string : filename (null-terminated)
+     *     string : mode (null-terminated)
+     * </pre>
+     *
+     * @param data the raw WRQ packet bytes received from the network
+     */
     public WRQPacket(byte[] data) {
         super(OP_WRQ);
         this.opcode = ByteBuffer.wrap(data, 0, 2).getShort();
@@ -20,6 +37,20 @@ public class WRQPacket extends TFTPPacket {
         this.filename = new String(data, start, end - start);
     }
 
+    /**
+     * Serializes this WRQ packet into a TFTP-compliant byte array.
+     * <p>
+     * Format:
+     * <pre>
+     *     2 bytes: opcode (2)
+     *     bytes : filename
+     *     1 byte: 0 terminator
+     *     bytes : mode (usually "octet")
+     *     1 byte: 0 terminator
+     * </pre>
+     *
+     * @return the encoded WRQ packet as a byte array
+     */
     @Override
     public byte[] serialize(){
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -33,6 +64,13 @@ public class WRQPacket extends TFTPPacket {
         return out.toByteArray();
     }
 
+    /**
+     * Finds the index of the next zero (null terminator) byte in the data array.
+     *
+     * @param data  the byte array to scan
+     * @param start the index at which to begin scanning
+     * @return the index of the next zero byte, or -1 if no terminator is found
+     */
     private int findZero(byte[] data, int start) {
         for (int i = start; i < data.length; i++)
             if (data[i] == 0) return i;
