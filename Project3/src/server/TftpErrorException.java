6d164f70a325579b2ae3200f17ddb20617014be7
JSmith1140 <168117141+JSmith1140@users.noreply.github.com>
2025-11-14 17:28:02 -0500

    Added javadoc comment to TftpErrorException.java

--- Project3/src/server/TftpErrorException.java
+++ Project3/src/server/TftpErrorException.java
@@ -1,6 +1,11 @@
 package server;
 
 public class TftpErrorException extends Exception {
+    /**
+     * Constructs a new {@code TftpErrorException} with the specified detail message.
+     *
+     * @param msg the detail message explaining the cause of the TFTP error
+     */
     public TftpErrorException(String msg) {
         super(msg);
     }
