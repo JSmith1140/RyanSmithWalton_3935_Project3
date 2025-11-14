eaa13a88aeabdae9634a49c160811486a0b74271
AdamWalton-0 <waltona@merrimack.edu>
2025-11-14 17:04:07 -0500

    Enhance TFTPErrorException with documentation comments
    Added comments to clarify the purpose of the TFTPErrorException class.

--- Project3/src/tftp/TFTPErrorException.java
+++ Project3/src/tftp/TFTPErrorException.java
@@ -1,11 +1,17 @@
 package tftp;
 
-
+/*  
+ * Exeption used for TFTP protocol errors
+ * Lets us distinguish TFTP errors from other IO errors
+ */
 public class TFTPErrorException extends Exception {
+
+
+//Construct a TFTP error with a message
     public TFTPErrorException(String message) {
         super(message);
     }
-
+//Construct a TFTP error with a message and cause
     public TFTPErrorException(String message, Throwable cause) {
         super(message, cause);
     }
